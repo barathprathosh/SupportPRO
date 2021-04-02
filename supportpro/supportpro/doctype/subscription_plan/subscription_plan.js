216a6508667304c7252e391c53d2470a12dc0fe1
Barath Prathosh M <barathprathosh@groupteampro.com>
2021-04-02 22:11:09 +0530

    AMC and Issue Form Created

---
+++ supportpro/supportpro/doctype/subscription_plan/subscription_plan.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, TeamPRO and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Subscription Plan', {
+	// refresh: function(frm) {
+
+	// }
+});
